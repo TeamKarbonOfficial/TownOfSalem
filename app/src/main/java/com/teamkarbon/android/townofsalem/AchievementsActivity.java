860f6a4b12f17d62e57f7eb7bf21d1cd37c04fbf
darkpolice <joe@teamkarbon.com>
2014-11-18 16:04:28 +0800

    Implemented AchievementsActivity TODO: Complete AchievementsDisplayActivity

---
+++ app/src/main/java/com/teamkarbon/android/townofsalem/AchievementsActivity.java
@@ -0,0 +1,58 @@
+package com.teamkarbon.android.townofsalem;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class AchievementsActivity extends Activity implements OnItemClickListener {
+
+    // Declare
+    ListView AchievementsListView;
+
+    String[] Achievements = { "General" ,"All Town" ,"Bodyguard", "Doctor", "Escort",
+            "Investigator", "Jailor", "Lookout", "Mayor", "Medium",
+            "Retributionist", "Sheriff", "Spy", "Transporter", "Veteran",
+            "Vigilante","All Mafia" , "Blackmailer", "Consigliere", "Consort", "Disguiser",
+            "Framer", "Janitor", "Godfather", "Mafioso","All Neutrals" , "Amnesiac",
+            "Survivor", "Witch", "Jester", "Executioner", "Arsonist",
+            "Serial Killer" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements);
+
+        AchievementsListView = (ListView) findViewById(R.id.RoleListView);
+        ArrayAdapter<String> Adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, Achievements);
+        AchievementsListView.setAdapter(Adapter);
+
+        AchievementsListView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_achievements, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View view, int arg2, long arg3) {
+        //Getting Text
+        TextView temp = (TextView) view;
+
+        //Intent
+        Intent DisplayIntent = new Intent(this, DisplayActivity.class);
+        DisplayIntent.putExtra("Role", temp.getText());
+        startActivity(DisplayIntent);
+    }
+}
--- app/src/main/java/com/teamkarbon/android/townofsalem/MainActivity.java
+++ app/src/main/java/com/teamkarbon/android/townofsalem/MainActivity.java
@@ -16,9 +16,7 @@ public class MainActivity extends Activity {
 	Button ButtonInno;
 	Button ButtonMafia;
 	Button ButtonNeutral;
-	Button ButtonCosmetic;
-	//Temp
-	Button ButtonKickStarter;
+	Button ButtonAchievements;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +28,23 @@ public class MainActivity extends Activity {
 		ButtonInno = (Button) findViewById(R.id.ButtonInno);
 		ButtonMafia = (Button) findViewById(R.id.ButtonMafia);
 		ButtonNeutral = (Button) findViewById(R.id.ButtonNeutral);
-		ButtonCosmetic = (Button) findViewById(R.id.ButtonCosmetic);
-		//Temp
-		ButtonKickStarter = (Button) findViewById(R.id.ButtonKickStarter);
+		ButtonAchievements = (Button) findViewById(R.id.ButtonAchievements);
 		
 		//Intent
 		Intent RoleIntent = new Intent(this, ListActivity.class);
 		Intent InnoIntent = new Intent(this, InnoActivity.class);
 		Intent MafiaIntent = new Intent(this, MafiaActivity.class);
 		Intent NeutralIntent = new Intent(this, NeutralActivity.class);
-		Intent CosmeticIntent = new Intent(this, CosmeticActivity.class);
-		//Temp
-		Intent KickStarterIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kickstarter.com/projects/2002718381/town-of-salem-mobile-steam-localization"));
+		Intent AchievementsIntent = new Intent(this, AchievementsActivity.class);
 		
 		addListenerOnButtonRole(RoleIntent);
 		addListenerOnButtonInno(InnoIntent);
 		addListenerOnButtonMafia(MafiaIntent);
 		addListenerOnButtonNeutral(NeutralIntent);
-		addListenerOnButtonCosmetic(CosmeticIntent);
-		addListenerOnButtonKickStarter(KickStarterIntent);
+        addListenerOnButtonAchievements(AchievementsIntent);
 	}
 
-	@Override
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
@@ -66,7 +59,7 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
+
 	private void addListenerOnButtonInno(final Intent InnoIntent) {
 		ButtonInno.setOnClickListener(new OnClickListener() {
 			@Override
@@ -75,7 +68,7 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
+
 	private void addListenerOnButtonMafia(final Intent MafiaIntent) {
 		ButtonMafia.setOnClickListener(new OnClickListener() {
 			@Override
@@ -84,7 +77,7 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
+
 	private void addListenerOnButtonNeutral(final Intent NeutralIntent) {
 		ButtonNeutral.setOnClickListener(new OnClickListener() {
 			@Override
@@ -93,27 +86,13 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
-	private void addListenerOnButtonCosmetic(final Intent CosmeticIntent) {
-		ButtonCosmetic.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				//startActivity(CosmeticIntent);
-				Toast.makeText(getApplicationContext(), "Coming Soon!", Toast.LENGTH_SHORT).show();
-			}
-		});
-	}
-	
-	//Temp
-	private void addListenerOnButtonKickStarter(final Intent KickStarterIntent) {
-		ButtonKickStarter.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				Toast.makeText(getApplicationContext(), "Thanks for supporting!", Toast.LENGTH_SHORT).show();
-				Toast.makeText(getApplicationContext(), "You Rule!", Toast.LENGTH_SHORT).show();
-				startActivity(KickStarterIntent);
-			}
-		});
-	}
 
+    private void addListenerOnButtonAchievements(final Intent AchievementsIntent) {
+        ButtonAchievements.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startActivity(AchievementsIntent);
+            }
+        });
+    }
 }
